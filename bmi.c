00dd19dfd6ddb014050dbf23abae696dd0b87108
komolo8 <marco.ignacio.litrenta@gmail.com>
2025-05-17 15:04:36 -0300

    Add files via upload

---
+++ bmi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	float peso, altura, imc;
+	
+	printf("Ingrese el peso en kg: ");
+	scanf("%f", &peso);
+	
+	printf("Ingrese la altura en metros: ");
+	scanf("%f", &altura);
+	
+	imc = peso / (altura * altura);
+	
+	printf("\nSu índice de masa corporal es: %.2f\n\n", imc);
+	
+	if(imc<18.5)
+		printf("usted esta bajo en peso");
+	
+	if(imc>=18.5 && imc<25)
+		printf("usted esta normal");
+	
+	if(imc>=25 && imc<30)
+		printf("usted esta con sobre peso");
+	
+	if(imc>=30)
+		printf("usted esta con obesidad");
+	return 0;
+}
