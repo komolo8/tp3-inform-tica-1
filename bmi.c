f54062b0beb352b6413de8ae25822e74ebb68dff
komolo8 <marco.ignacio.litrenta@gmail.com>
2025-05-17 17:59:01 -0300

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -11,18 +11,13 @@ int main() {
 	
 	imc = peso / (altura * altura);
 	
-	printf("\nSu índice de masa corporal es: %.2f\n\n", imc);
+	printf("\nSu Ã­ndice de masa corporal es: %.2f\n\n", imc);
 	
-	if(imc<18.5)
-		printf("usted esta bajo en peso");
-	
-	if(imc>=18.5 && imc<25)
-		printf("usted esta normal");
-	
-	if(imc>=25 && imc<30)
-		printf("usted esta con sobre peso");
-	
-	if(imc>=30)
-		printf("usted esta con obesidad");
+    printf("\nIndice         | Condicion\n");
+    printf("-----------------------------\n");
+    printf("<18.5          | Bajo peso\n");
+    printf("18.5 a 24.9    | Normal\n");
+    printf("25.0 a 29.9    | Sobrepeso\n");
+    printf(">=30           | Obesidad\n");
 	return 0;
 }
